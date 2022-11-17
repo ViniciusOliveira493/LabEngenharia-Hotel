c345f1e1fcfca021d0e4aeb4bd78a23f5628b694
viniciusOliveira493 <vinilimadeoliveira@hotmail.com>
2022-11-16 21:17:01 -0300

    Arquivo de texto contendo os formatos dos JSONs criado

--- src/main/resources/createBd.sql
+++ src/main/resources/createBd.sql
@@ -10,25 +10,3 @@ INSERT INTO tbTipoDocumento(nomeTipo)
         ('RG')
         ,('CPF')
         , ('ITIN number')
-
-Insert into tbVaga(estacionamento,numVaga,descricao,valorDiaria,tipo)
-    VALUES
-        ('E01',01,'vaga para carro',15,1)
-
-select * from tbPessoa
-INSERT INTO tbPessoa(documento,email,nome,senha,telefone,funcao,tipoDocumento)
-    VALUES
-        ('11111111111','adminmaster@gmail.com','admin','e10adc3949ba59abbe56e057f20f883e','0000000000',0,1)
-
-SELECT  
-    pessoa0_.documento 
-    ,pessoa0_.tipoDocumento 
-    ,pessoa0_.email 
-    ,pessoa0_.funcao 
-    ,pessoa0_.nome 
-    ,pessoa0_.senha 
-    ,pessoa0_.telefone
-from 
-    tbPessoa pessoa0_
-WHERE
-    funcao != 3
