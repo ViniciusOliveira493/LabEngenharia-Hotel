29c1f550e31cefbc69c6746a672e15e072666b92
viniciusOliveira493 <vinilimadeoliveira@hotmail.com>
2022-12-03 15:19:10 -0300

    Problema com CORS resolvido

--- src/main/java/br/edu/fateczl/Hotel/WebConfig.java
+++ src/main/java/br/edu/fateczl/Hotel/WebConfig.java
@@ -1,16 +1,50 @@
 package br.edu.fateczl.Hotel;
 
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
-
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class WebConfig implements WebMvcConfigurer,Filter {
+	String origem = "http://localhost:8081";
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
     }
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+		
+		resp.setHeader("Access-Control-Allow-Origin", origem);
+        resp.setHeader("Access-Control-Allow-Credentials", "true");
+		
+		if ("OPTIONS".equals(req.getMethod()) && origem.equals(req.getHeader("Origin"))) {
+			resp.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
+        	resp.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+        	resp.setHeader("Access-Control-Max-Age", "3600");
+			
+			resp.setStatus(HttpServletResponse.SC_OK);
+		} else {
+			chain.doFilter(req, resp);
+		}
+		
+	}
 }
